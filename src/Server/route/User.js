8f267fea3b4a24e7a7b26cc88c2da90a83d32c3c
NguyenTien243 <nguyenquoctien243@gmail.com>
2021-10-28 20:44:05 +0700

    merge lần 1

--- src/Server/route/User.js
+++ src/Server/route/User.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const productController = require('../controller/UserController');
-router.get('/:id', productController.find);
-router.post('/:id', productController.update);
-router.get('/all', productController.show);
+const userController = require('../controller/UserController');
+router.get('/info/:id', userController.info);
+// router.put('/edit/:bidanh', userController.edit); 
+// router.delete('/delete/:id', userController.delete); 
+
 
 
 module.exports=router;
