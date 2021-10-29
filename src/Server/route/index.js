6112b5b4d8aa9ed6b0d7d75a9e69bfe05eb5e2e0
souldippy <56272004+souldippy@users.noreply.github.com>
2021-10-29 20:27:38 +0700

    fdf
    fdfdfd

--- src/Server/route/index.js
+++ src/Server/route/index.js
@@ -3,11 +3,13 @@ const bookRouter = require('./book');
 const userRouter = require('./user');
 const hoadonRouter = require('./HoaDon');
 const adminRouter = require('./admin');
+const signupRouter = require('./signup');
 function route(app) {
     app.use('/book', bookRouter);
     app.use('/user', userRouter);
     app.use('/admin', adminRouter);
     app.use('/hoadon', hoadonRouter);
+    app.use('/signup', signupRouter);
     // với re là reqiure và res là response
 
 
