fbdb00b40ef986c6f467a4f9af3c871be1fc15db
souldippy <56272004+souldippy@users.noreply.github.com>
2021-11-15 14:43:48 +0700

    Commit lại FE và middleware check lỗi BE

--- front-end/src/components/Layout/index.js
+++ front-end/src/components/Layout/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Header } from './Header'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 /**
 * @author
 * @function Layout
@@ -10,8 +11,26 @@ export const Layout = (props) => {
     return (
         <>
             <Header />
-                {props.children}
-
+            {
+                props.sidebar ?
+                    <Container fluid>
+                        <Row>
+                            <Col md={2} className="sidebar">
+                                <ul>
+                                    <li><NavLink to={`/`}>Home</NavLink></li>
+                                    <li><NavLink to={`/book`}>Quản lý sách</NavLink></li>
+                                    <li><NavLink to={`/orders`}>Quản lý đơn hàng</NavLink></li>
+                                </ul>
+                            </Col>
+                            <Col md={10} style={{ marginLeft: 'auto' }}>
+                            {props.children}
+                            </Col>
+                        </Row>
+                    </Container>
+                    :
+                    props.children
+                    
+            }
 
         </>
     )
--- front-end/src/helpers/axios.js
+++ front-end/src/helpers/axios.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 import {api} from '../urlConfig'
+
+const token = window.localStorage.getItem('token');
+
 const axiosIntance = axios.create({
     baseURL: api,
-    // header:{
-    //     'Authorization':''
-    // }
+    headers:{
+        'Authorization': token? `Bearer ${token}` : ''
+    }
 });
 export default axiosIntance;
