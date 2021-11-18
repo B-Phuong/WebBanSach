96ade2e1f23de27e81c9941068097ff7c8feddb2
NguyenTien243 <nguyenquoctien243@gmail.com>
2021-11-18 20:08:20 +0700

    tạm sữa lỗi cart

--- src/Server/controller/CartController.js
+++ src/Server/controller/CartController.js
@@ -3,105 +3,68 @@ const Book = require("../model/Book");
 const { mutipleMongoseToObject } = require("../util/mongoose");
 
 class CartController {
+  // function
+
   // thêm sản phẩm vào giỏ hàng
   //[PUT] /cart/:iduser
   async addBookToCart(req, res, next) {
-    console.log(req.body.user._id.$oid);
-
-    var {maSach,tenSach,hinhAnh,soLuong,giaTien,giamGia} = req.body
-
-    await User.findById({ _id: req.body.user._id.$oid }) // sau này có token thì sữa lại lấy user từ token
-      .then((user) => { 
-        if (!user)
-        return res.status(404).json({
-          status: 404,
-          message: "không tìm thấy Id User",
-        });
-        else {
-          var book = {
-            maSach: maSach,
-            tenSach: tenSach,
-            hinhAnh: hinhAnh,
-            soLuong: soLuong,
-            giaGoc: giaTien,
-            giamGia: giamGia,
-          };
-          console.log(book);
-          book.tongTien = giaTien - (giaTien * giamGia) / 100) * soLuong;
     
-          // if(!data.gioHang) // nếu giỏ hàng chưa có thì khởi tạo
-          //   giohang = []
-          var giohangnew;
-          if (!user.gioHang) giohangnew = [];
-          else giohangnew = user.gioHang;
-          //kiểm tra nếu sản phẩm có ở giỏ hàng trước rồi thì cập nhật số lượng
-          // if(user.gioHang[id])
-          var isItemExist = user.gioHang.some((item, index) => {
-            return item.maSach === book.maSach;
-          });
-          
-          if (isItemExist) {
-            // nếu đã có trong giỏ hàng rồi thì cập nhật số lượng
-            var findBookInCart = user.gioHang.filter((item, index) => {
-              return item.maSach === book.maSach;
-            });
-            var soLuongBanDauTrongGio = findBookInCart.soLuong;
-            var SoLuongCanThemVaoGio = book.soLuong;
-            Book.findById({ _id: book.maSach }).then((data) => {
-              if (!data) {
-                res.status(400).json({ message: "Lỗi khi tìm sách"});
-              } 
-              else {
-                if ( // kiểm tra số lượng cần thêm vào đủ không
-                  data.soLuongConLai >=
-                  SoLuongCanThemVaoGio + soLuongBanDauTrongGio
-                ) {
-                  // đủ thì thêm
-                  var updateNumberBook = new Promise(function (resolve, reject) {
-                    Book.updateOne({ _id: book.maSach },{soLuongConLai: data.soLuongConLai - SoLuongCanThemVaoGio}).then((result) => {
-                      resolve(result)
-                    }).catch((err) => {reject(err)});
-                  });
-                  var newCart = user.gioHang.map(x=> {return x})
-                  // cập nhật lại số lượng
-                  for(index in newCart){
-                    if(newCart[index].maSach == book.maSach)
-                    {
-                      newCart[index].soLuong = soLuongBanDauTrongGio + SoLuongCanThemVaoGio
-                      newCart[index].tongTien = (newCart[index].giaTien - (newCart[index].giaTien * newCart[index].giamGia) / 100) * newCart[index].soLuong;
-                      break
-                    } 
-                  }
-                  var updateBookInCart = new Promise(function (resolve, reject) {
-                    User.updateOne({ _id: req.body.user._id.$oid },{gioHang:newCart}).then((updated) => {
-                      resolve(updated)
-                    });
-                  });
 
-                  Promise.all([updateNumberBook,updateBookInCart])
-                  .then((bookupdated,cartupdated)=>{
-                    res.status(200).json({ message: 'thêm thành công vào giỏ hàng'})
-                  })
-                  .catch(err => {return res.status(500).json({ message: err})})
-                }
-                else
-                {
-                  return res
-                  .status(400)
-                  .json({
-                    message:
-                      "Số lượng sách không đủ để thêm vào giỏ hàng, vui lòng thử lại",
-                  });
-                }
-              }
-            })
-            .catch((err) => res.status(400).json({ message: err }));
-         }
+    var { maSach, tenSach, hinhAnh, soLuong, giaTien, giamGia } = req.body;
 
-        }
-  })
+    // kiểm tra mã sách hợp lệ Không
+    Book.findById({_id:maSach})
+    .then((checkIdBook) => {
+      if(!checkIdBook)
+      return res.status(200).json({ message:'ăn lìn rồi'})
+    })
+    .catch(err => {messase: "Lỗi khi tìm mã sách"})
+
+    
+
+
+    // await User.findById({ _id: req.body.user._id.$oid }) // sau này có token thì sữa lại lấy user từ token
+    //   .then((user) => {
+    //     if (!user)
+    //       return res.status(404).json({
+    //         status: 404,
+    //         message: "không tìm thấy Id User",
+    //       });
+    //       else {
+    //         // nếu giỏ hàng chưa có thì khởi tạo
+    //         var giohangnew;
+    //         if (!user.gioHang) giohangnew = [];
+    //         else giohangnew = user.gioHang;
+    //         //kiểm tra nếu sản phẩm có ở giỏ hàng trước rồi thì cập nhật số lượng
+    //         // if(user.gioHang[id])
+    //         var isItemExist = user.gioHang.some((item, index) => {
+    //           return item.maSach === maSach;
+    //         });
+    //         if (isItemExist) { // item đã có trong giỏ hàng trước đó --> cập nhật số lượng
+    //           var findBookInCart = user.gioHang.filter((item, index) => {
+    //             return item.maSach === maSach;
+    //           });
+    //           let soLuongBanDauTrongGio = findBookInCart[0].soLuong;
+    //           var SoLuongCanThemVaoGio = soLuong;
+    //           res.status(200).json(maSach)
+    //           // // kiểm tra còn số sách trong giỏ hàng sau thêm có vượt số lượng hiện có của sách không
+    //           // Book.findById({ _id: maSach }).then((data) => {
+    //           //   if (!data) {
+    //           //     res.status(400).json({ message: "Lỗi khi tìm sách"});
+    //           //   } 
+    //           //   else{
+    //           //     res.status(200).json(data)
+    //           //   }
+
+    //           // })
+    //         }
+
+
+    //       }
+            
+    //   })
+    //   .catch((err) => res.status(400).json({ message: err }));
+  }
 }
-}
-
 
 module.exports = new CartController();
