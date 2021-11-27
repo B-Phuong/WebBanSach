71cc7fa8e3d81f3d0efdaf4f64dd3883a1620de0
NguyenTien243 <nguyenquoctien243@gmail.com>
2021-11-27 14:08:20 +0700

    tạm

--- front-end/src/containers/BookDetail/detail.js
+++ front-end/src/containers/BookDetail/detail.js
@@ -1,5 +1,4 @@
-
-import React, { useEffect } from 'react';
+import React, { useEffect , useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDetailBook } from '../../actions';
 import IndexHome from '../../components/Layout/Header/indexHome'
@@ -8,6 +7,7 @@ import './detail.css'
 const BookDetail = (props) => {
     const dispatch = useDispatch();
     const book = useSelector(state => state.book.bookDetails);
+    const [qty, setQty] = useState(1);
     // const books = [{ tenSach: 'sách1' }, { tenSach: 'ténach2' }]
     // const { page } = product;
     useEffect(() => {
@@ -26,7 +26,18 @@ const BookDetail = (props) => {
         // }
         dispatch(getDetailBook(id));
     }, []);
-
+    const AddQuatity= (e) => {
+        if (qty + 1 > book.soLuongBan) return;
+        setQty(qty + 1);
+    }
+    const SubQuatity= (e) => {
+        if (qty <= 1) return;
+    setQty(qty - 1);
+     }
+     const ChangeQuatity= (e) => {
+       
+        console.log("e.value")
+     }
     return (
         <>
             <IndexHome />
@@ -39,9 +50,9 @@ const BookDetail = (props) => {
                     <div id='author'>
                         <span>Tác giả:<> <b>{book.tacGia}</b></></span>
                     </div>
-                    <div id='publisher'>
+                    {/* <div id='publisher'>
                         <span>Nhà xuất bản:<> <b>{book.maNhaXuatBan.tenNhaXuatBan}</b></></span>
-                    </div>
+                    </div> */}
 
                 </div>
 
@@ -53,9 +64,9 @@ const BookDetail = (props) => {
                     </div>
                     <div className="buttons_added">
                         <h4 >Số lượng </h4>
-                        <input className="minus is-form" type='button' value='-'></input>
-                        <input className="input-qty" type='number' value='1'></input>
-                        <input className="plus is-form" type='button' value='+'></input>
+                        <input className="minus is-form" type='button' value='-'onClick={SubQuatity}></input>
+                        <input className="input-qty" type='number' value = {qty} min="1" max="5" onChange={(e) => ChangeQuatity(e.target.value)}></input>
+                        <input className="plus is-form" type='button' value='+' onClick={AddQuatity}></input>
                     </div>
                     <div><h4 style={{ margin: '20px' }}>Số lượng bán: {book.soLuongBan}</h4></div>
                     <div className='button'>
--- front-end/src/containers/Cart/index.js
+++ front-end/src/containers/Cart/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import IndexHome from '../../components/Layout/Header/indexHome'
 import Layout from "../../components/Layout";
 import Card from "../../components/UI/Card";
 import CartItem from "./CartItem";
