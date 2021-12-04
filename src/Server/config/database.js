99683e8dde88862a7cc1de46bdb3a0f8ff000658
NguyenTien243 <nguyenquoctien243@gmail.com>
2021-12-04 12:50:58 +0700

    abc

--- src/Server/config/database.js
+++ src/Server/config/database.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const env = require('dotenv');
-env.config()
+env.config() //a
+require()
 async function connect() { 
     try {
+        
         await mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.an7m2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
            useNewUrlParser: true,
            useUnifiedTopology: true
